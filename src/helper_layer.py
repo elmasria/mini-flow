c00fedbb628cd1eb310a74aeb1ba50463872e163
Ahmad EL Masri <ahmadjelmasri@gmail.com>
2017-01-09 12:23:27 +0200

    feat: implement Stochastic Gradient Descent

--- src/helper_layer.py
+++ src/helper_layer.py
@@ -96,3 +96,19 @@ def forward_and_backward(graph):
     # see: https://docs.python.org/2.3/whatsnew/section-slices.html
     for n in graph[::-1]:
         n.backward()
+
+def sgd_update(trainables, learning_rate=1e-2):
+    """
+    Updates the value of each trainable with SGD.
+
+    Arguments:
+
+        `trainables`: A list of `Input` Layers representing weights/biases.
+        `learning_rate`: The learning rate.
+    """
+    # TODO: update all the `trainables` with SGD
+    # You can access and assign the value of a trainable with `value` attribute.
+    # Example:
+    for t in trainables:
+        partial = t.gradients[t]
+        t.value = t.value - learning_rate*partial
